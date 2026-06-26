95abc3c994fe77f57ada616b3736d7625d336569
Marcelo Saldanha <marcelosfilho.fundador@yellowbagedu.com>
2026-06-26 00:50:03 -0300

    fix: fix code in prismaClient.ts

--- src/database/prismaClient.ts
+++ src/database/prismaClient.ts
@@ -4,3 +4,5 @@ import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
+
+export { prisma };
