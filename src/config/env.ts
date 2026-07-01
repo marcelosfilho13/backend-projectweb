ce74c0dc218e99e2644e8bd5d25d2fe13ade6ed0
Marcelo Saldanha <marcelosfilho.fundador@yellowbagedu.com>
2026-06-30 23:53:40 -0300

    teste

--- src/config/env.ts
+++
@@ -1,3 +0,0 @@
-import dotenv from "dotenv";
-//* Carregar as varoiáveis de ambiente do arquivo .env
-dotenv.config();
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { routes } from "./routes";
+import { prisma } from "./database/prismaClient";
 
 const app = express();
 
@@ -19,4 +20,26 @@ const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => {
   console.log(`🚀 Servidor backend inicializado com sucesso!`);
   console.log(`📡 Escutando requisições na URL: http://localhost:${PORT}`);
+});
+
+//! Rota de Healthcheck para testar a saúde da API e do Banco na Nuvem
+app.get("/healthcheck", async (req, res) => {
+  try {
+    // Indicamos ao TypeScript que o retorno esperado é um array genérico <unknown[]>
+    await prisma.$queryRaw<unknown[]>`SELECT 1`;
+
+    return res.status(200).json({
+      status: "online",
+      database: "connected",
+      timestamp: new Date(),
+      message: "SADE API e Banco no Render estão operacionais! 🚀",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: "error",
+      database: "disconnected",
+      message: "A API está de pé, mas falhou ao conectar com o banco do Render.",
+      error: error instanceof Error ? error.message : error
+    });
+  }
 });
